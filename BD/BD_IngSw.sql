ebbf0d7cafcb47c9bc82165bf04b05109c25438b
Alan Alexis Suarez Moreno <152936639+alexis25091@users.noreply.github.com>
2026-04-15 21:30:23 -0600

    cambio de las tablas ya finales sin uso de IA

--- BD/BD_IngSw.sql
+++ BD/BD_IngSw.sql
@@ -3,51 +3,43 @@
     tal cual el diagrama normalizado.
 */
 
-CREATE DATABASE gestor_tareas_v2;
-USE gestor_tareas_v2;
+CREATE DATABASE NextTask;
+USE NextTask;
 
-CREATE TABLE usuario (
-    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
-    nombre_usuario VARCHAR(50) NOT NULL,
-    correo VARCHAR(100) NOT NULL UNIQUE,
-    contrasena VARCHAR(255) NOT NULL
+CREATE TABLE usuarios (
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (email)
 );
 
-CREATE TABLE materia (
-    id_materia INT AUTO_INCREMENT PRIMARY KEY,
-    id_usuario INT NOT NULL,
-    nombre_materia VARCHAR(100) NOT NULL,
-    dificultad INT NOT NULL,
+CREATE TABLE materias (
+    id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    nombre VARCHAR(100) NOT NULL,
     color VARCHAR(100) NOT NULL,
-    activo BOOLEAN DEFAULT TRUE, -- Agregado para que no se elimine y se mantenga en el historial
-    FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
+    estado TINYINT(1) DEFAULT 1,
+    dificultad INT DEFAULT 1,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_materias_usuario
+        FOREIGN KEY (user_id) REFERENCES usuarios(id)
 );
 
-CREATE TABLE estado (
-    id_estado INT PRIMARY KEY,
-    nombre_estado VARCHAR(20) NOT NULL
-);
-
-CREATE TABLE tarea (
-    id_tarea INT AUTO_INCREMENT PRIMARY KEY,
-    id_materia INT NOT NULL,
-    -- titulo VARCHAR(100) NOT NULL, 
-    descripcion TEXT,
-    dificultad INT NOT NULL CHECK (dificultad BETWEEN 1 AND 10),
-    fecha_entrega DATETIME NOT NULL,
-    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- AGREGADO
-    id_estado INT NOT NULL,
-    FOREIGN KEY (id_materia) REFERENCES materia(id_materia)
-		ON DELETE CASCADE,
-    FOREIGN KEY (id_estado) REFERENCES estado(id_estado)
-);
-
-CREATE TABLE priorizacion (
-    id_priorizacion INT AUTO_INCREMENT PRIMARY KEY,
-    id_tarea INT NOT NULL,
-    puntaje_final DECIMAL(5,2),
-    nivel_prioridad ENUM('baja','media','alta','muy alta'),
-	fecha_calculo TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id_tarea) REFERENCES tarea(id_tarea)
+CREATE TABLE tareas (
+    id INT NOT NULL AUTO_INCREMENT,
+    dificultad TINYINT NOT NULL,
+    fecha_limite DATETIME NOT NULL,
+    detalles TEXT,
+    estado ENUM('asignada', 'en_proceso', 'completada') NOT NULL,
+    creado_en TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+    user_id INT NOT NULL,
+    materia_id INT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_tareas_usuario
+        FOREIGN KEY (user_id) REFERENCES usuarios(id),
+    CONSTRAINT fk_tareas_materia
+        FOREIGN KEY (materia_id) REFERENCES materias(id)
         ON DELETE CASCADE
 );
